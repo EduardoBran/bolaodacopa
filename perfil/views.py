8ffb13b60da4dcef2fda92169d20d4a27555d27f
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-11-04 20:14:03 -0300

    implementando register | login | logout

--- perfil/views.py
+++ perfil/views.py
@@ -1,9 +1,65 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render
+
+from .forms import RegistrationForm
 
 
 def loginPage(request):
-    return render(request, 'perfil/login.html')
+    if request.user.is_authenticated:
+        # add message
+        return redirect('main:pageindex')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    # add message
+                    return redirect('main:pageindex')
+                else:
+                    return render(request, 'perfil/login.html', {"error": "Sua conta está desabilitada."})
+            else:
+                return render(request, 'perfil/login.html', {"error": "Usuário ou senha inválidos. Tente novamente."}) 
+        else:
+            return render(request, 'perfil/login.html')
+
+
+def logoutPage(request):
+    if request.user.is_authenticated:
+        logout(request)
+        # add message
+        return redirect('main:pageindex')
+    else:
+        # add message
+        return redirect('main:pageindex')
+
 
 
 def registerPage(request):
-    return render(request, 'perfil/register.html')
+    if request.user.is_authenticated:
+        # add message
+        return redirect('main:pageindex')
+    else:
+        if request.method == 'POST':
+            form = RegistrationForm(request.POST or None)
+
+            if form.is_valid():
+                user = form.save()
+
+                raw_password = form.cleaned_data.get('password1')
+
+                user = authenticate(username=user.username, password=raw_password)
+
+                if user is not None:
+                    login(request, user)
+                    # add message
+                    return redirect('main:pageindex')
+        else:
+            form = RegistrationForm()
+
+        context = {'form': form}
+        return render(request, 'perfil/register.html', context)

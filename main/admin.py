53a30563ff522611e67b00d49fe1921baf145710
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-11-07 20:35:57 -0300

    add models e admin para os grupos que faltavam

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import GrupoA, GrupoB
+from .models import *
 
 
 class GrupoAAdmin(admin.ModelAdmin):
@@ -16,7 +16,54 @@ class GrupoBAdmin(admin.ModelAdmin):
     list_display_links = (
         'usuario',
     )
+
+class GrupoCAdmin(admin.ModelAdmin):
+    list_display = ('usuario',)
     
+    list_display_links = (
+        'usuario',
+    )
+
+class GrupoDAdmin(admin.ModelAdmin):
+    list_display = ('usuario',)
+    
+    list_display_links = (
+        'usuario',
+    )
+
+class GrupoEAdmin(admin.ModelAdmin):
+    list_display = ('usuario',)
+    
+    list_display_links = (
+        'usuario',
+    )
+    
+class GrupoFAdmin(admin.ModelAdmin):
+    list_display = ('usuario',)
+    
+    list_display_links = (
+        'usuario',
+    )
+    
+class GrupoGAdmin(admin.ModelAdmin):
+    list_display = ('usuario',)
+    
+    list_display_links = (
+        'usuario',
+    )
+
+class GrupoHAdmin(admin.ModelAdmin):
+    list_display = ('usuario',)
+    
+    list_display_links = (
+        'usuario',
+    )
     
 admin.site.register(GrupoA, GrupoAAdmin)
 admin.site.register(GrupoB, GrupoBAdmin)
+admin.site.register(GrupoC, GrupoCAdmin)
+admin.site.register(GrupoD, GrupoDAdmin)
+admin.site.register(GrupoE, GrupoEAdmin)
+admin.site.register(GrupoF, GrupoFAdmin)
+admin.site.register(GrupoG, GrupoGAdmin)
+admin.site.register(GrupoH, GrupoHAdmin)
--- main/models.py
+++ main/models.py
@@ -65,3 +65,180 @@ class GrupoB(Base):
 
     def __str__(self):
         return f'Grupo B'
+    
+
+class GrupoC(Base):
+    usuario = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name='Usuário')
+    selecao1 = models.CharField(max_length=200, verbose_name='Seleção 1', default='Argentina', blank=True, null=True)
+    selecao2 = models.CharField(max_length=200, verbose_name='Seleção 2', default='Arábia Saudita', blank=True, null=True)
+    selecao3 = models.CharField(max_length=200, verbose_name='Seleção 3', default='México', blank=True, null=True)
+    selecao4 = models.CharField(max_length=200, verbose_name='Seleção 4', default='Polônia', blank=True, null=True)
+    res1 = models.SmallIntegerField(blank=True, null=True)
+    res2 = models.SmallIntegerField(blank=True, null=True)
+    res3 = models.SmallIntegerField(blank=True, null=True)
+    res4 = models.SmallIntegerField(blank=True, null=True)
+    res5 = models.SmallIntegerField(blank=True, null=True)
+    res6 = models.SmallIntegerField(blank=True, null=True)
+    res7 = models.SmallIntegerField(blank=True, null=True)
+    res8 = models.SmallIntegerField(blank=True, null=True)
+    res9 = models.SmallIntegerField(blank=True, null=True)
+    res10 = models.SmallIntegerField(blank=True, null=True)
+    res11 = models.SmallIntegerField(blank=True, null=True)
+    res12 = models.SmallIntegerField(blank=True, null=True)
+    
+    class Meta:
+        verbose_name = 'Grupo C'
+        verbose_name_plural = 'Grupo C'
+
+    def __str__(self):
+        return f'Grupo C'
+    
+    
+class GrupoD(Base):
+    usuario = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name='Usuário')
+    selecao1 = models.CharField(max_length=200, verbose_name='Seleção 1', default='França', blank=True, null=True)
+    selecao2 = models.CharField(max_length=200, verbose_name='Seleção 2', default='Austrália', blank=True, null=True)
+    selecao3 = models.CharField(max_length=200, verbose_name='Seleção 3', default='Dinamarca', blank=True, null=True)
+    selecao4 = models.CharField(max_length=200, verbose_name='Seleção 4', default='Tunísia', blank=True, null=True)
+    res1 = models.SmallIntegerField(blank=True, null=True)
+    res2 = models.SmallIntegerField(blank=True, null=True)
+    res3 = models.SmallIntegerField(blank=True, null=True)
+    res4 = models.SmallIntegerField(blank=True, null=True)
+    res5 = models.SmallIntegerField(blank=True, null=True)
+    res6 = models.SmallIntegerField(blank=True, null=True)
+    res7 = models.SmallIntegerField(blank=True, null=True)
+    res8 = models.SmallIntegerField(blank=True, null=True)
+    res9 = models.SmallIntegerField(blank=True, null=True)
+    res10 = models.SmallIntegerField(blank=True, null=True)
+    res11 = models.SmallIntegerField(blank=True, null=True)
+    res12 = models.SmallIntegerField(blank=True, null=True)
+    
+    class Meta:
+        verbose_name = 'Grupo D'
+        verbose_name_plural = 'Grupo D'
+
+    def __str__(self):
+        return f'Grupo D'
+    
+
+class GrupoE(Base):
+    usuario = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name='Usuário')
+    selecao1 = models.CharField(max_length=200, verbose_name='Seleção 1', default='Espanha', blank=True, null=True)
+    selecao2 = models.CharField(max_length=200, verbose_name='Seleção 2', default='Costa Rica', blank=True, null=True)
+    selecao3 = models.CharField(max_length=200, verbose_name='Seleção 3', default='Alemanha', blank=True, null=True)
+    selecao4 = models.CharField(max_length=200, verbose_name='Seleção 4', default='Japão', blank=True, null=True)
+    res1 = models.SmallIntegerField(blank=True, null=True)
+    res2 = models.SmallIntegerField(blank=True, null=True)
+    res3 = models.SmallIntegerField(blank=True, null=True)
+    res4 = models.SmallIntegerField(blank=True, null=True)
+    res5 = models.SmallIntegerField(blank=True, null=True)
+    res6 = models.SmallIntegerField(blank=True, null=True)
+    res7 = models.SmallIntegerField(blank=True, null=True)
+    res8 = models.SmallIntegerField(blank=True, null=True)
+    res9 = models.SmallIntegerField(blank=True, null=True)
+    res10 = models.SmallIntegerField(blank=True, null=True)
+    res11 = models.SmallIntegerField(blank=True, null=True)
+    res12 = models.SmallIntegerField(blank=True, null=True)
+    
+    class Meta:
+        verbose_name = 'Grupo E'
+        verbose_name_plural = 'Grupo E'
+
+    def __str__(self):
+        return f'Grupo E'
+    
+    
+class GrupoF(Base):
+    usuario = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name='Usuário')
+    selecao1 = models.CharField(max_length=200, verbose_name='Seleção 1', default='Bélgica', blank=True, null=True)
+    selecao2 = models.CharField(max_length=200, verbose_name='Seleção 2', default='Canadá', blank=True, null=True)
+    selecao3 = models.CharField(max_length=200, verbose_name='Seleção 3', default='Marrocos', blank=True, null=True)
+    selecao4 = models.CharField(max_length=200, verbose_name='Seleção 4', default='Croácia', blank=True, null=True)
+    res1 = models.SmallIntegerField(blank=True, null=True)
+    res2 = models.SmallIntegerField(blank=True, null=True)
+    res3 = models.SmallIntegerField(blank=True, null=True)
+    res4 = models.SmallIntegerField(blank=True, null=True)
+    res5 = models.SmallIntegerField(blank=True, null=True)
+    res6 = models.SmallIntegerField(blank=True, null=True)
+    res7 = models.SmallIntegerField(blank=True, null=True)
+    res8 = models.SmallIntegerField(blank=True, null=True)
+    res9 = models.SmallIntegerField(blank=True, null=True)
+    res10 = models.SmallIntegerField(blank=True, null=True)
+    res11 = models.SmallIntegerField(blank=True, null=True)
+    res12 = models.SmallIntegerField(blank=True, null=True)
+    
+    class Meta:
+        verbose_name = 'Grupo F'
+        verbose_name_plural = 'Grupo F'
+
+    def __str__(self):
+        return f'Grupo F'
+
+
+class GrupoG(Base):
+    usuario = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name='Usuário')
+    selecao1 = models.CharField(max_length=200, verbose_name='Seleção 1', default='Brasil', blank=True, null=True)
+    selecao2 = models.CharField(max_length=200, verbose_name='Seleção 2', default='Sérvia', blank=True, null=True)
+    selecao3 = models.CharField(max_length=200, verbose_name='Seleção 3', default='Suiça', blank=True, null=True)
+    selecao4 = models.CharField(max_length=200, verbose_name='Seleção 4', default='Camarões', blank=True, null=True)
+    res1 = models.SmallIntegerField(blank=True, null=True)
+    res2 = models.SmallIntegerField(blank=True, null=True)
+    res3 = models.SmallIntegerField(blank=True, null=True)
+    res4 = models.SmallIntegerField(blank=True, null=True)
+    res5 = models.SmallIntegerField(blank=True, null=True)
+    res6 = models.SmallIntegerField(blank=True, null=True)
+    res7 = models.SmallIntegerField(blank=True, null=True)
+    res8 = models.SmallIntegerField(blank=True, null=True)
+    res9 = models.SmallIntegerField(blank=True, null=True)
+    res10 = models.SmallIntegerField(blank=True, null=True)
+    res11 = models.SmallIntegerField(blank=True, null=True)
+    res12 = models.SmallIntegerField(blank=True, null=True)
+    
+    class Meta:
+        verbose_name = 'Grupo G'
+        verbose_name_plural = 'Grupo G'
+
+    def __str__(self):
+        return f'Grupo G'
+
+
+class GrupoH(Base):
+    usuario = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name='Usuário')
+    selecao1 = models.CharField(max_length=200, verbose_name='Seleção 1', default='Portugal', blank=True, null=True)
+    selecao2 = models.CharField(max_length=200, verbose_name='Seleção 2', default='Gana', blank=True, null=True)
+    selecao3 = models.CharField(max_length=200, verbose_name='Seleção 3', default='Uruguai', blank=True, null=True)
+    selecao4 = models.CharField(max_length=200, verbose_name='Seleção 4', default='Coréia do Sul', blank=True, null=True)
+    res1 = models.SmallIntegerField(blank=True, null=True)
+    res2 = models.SmallIntegerField(blank=True, null=True)
+    res3 = models.SmallIntegerField(blank=True, null=True)
+    res4 = models.SmallIntegerField(blank=True, null=True)
+    res5 = models.SmallIntegerField(blank=True, null=True)
+    res6 = models.SmallIntegerField(blank=True, null=True)
+    res7 = models.SmallIntegerField(blank=True, null=True)
+    res8 = models.SmallIntegerField(blank=True, null=True)
+    res9 = models.SmallIntegerField(blank=True, null=True)
+    res10 = models.SmallIntegerField(blank=True, null=True)
+    res11 = models.SmallIntegerField(blank=True, null=True)
+    res12 = models.SmallIntegerField(blank=True, null=True)
+    
+    class Meta:
+        verbose_name = 'Grupo H'
+        verbose_name_plural = 'Grupo H'
+
+    def __str__(self):
+        return f'Grupo H'
+    
+
+    
+
+    
+
+    
+
+    
